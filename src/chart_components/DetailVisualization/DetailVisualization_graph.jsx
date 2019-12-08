ee2fb8d80227eb5234f30e33d5c372ce7225bbdf
jinhyeok <persona5173@gmail.com>
2019-12-08 19:22:53 +0900

    Develop graph extention

--- src/chart_components/DetailVisualization/DetailVisualization_graph.jsx
+++ src/chart_components/DetailVisualization/DetailVisualization_graph.jsx
@@ -14,7 +14,43 @@ const myConfig = {
     highlightColor: 'lightblue',
   },
 };
-
+const ddata = {
+  nodes: [
+    { id: '장르', color: 'blue', size: 400 },
+    { id: '개봉일', color: 'yellow', size: 400 },
+    { id: '감독', color: 'green', size: 400 },
+    { id: '배우', color: 'black', size: 400 },
+    { id: '회사', color: 'purple', size: 400 },
+    { id: '조커(Joker)', color: 'red', size: 1000 },
+    { id: '20191002', color: 'yellow' },
+    { id: '액션', color: 'blue' },
+    { id: '드라마', color: 'blue' },
+    { id: '스릴러', color: 'blue' },
+    { id: '토드 필립스', color: 'green' },
+    { id: '호아킨 피닉스(Joaquin Phoenix)', color: 'black' },
+    { id: '재지 비츠(Zazie Beetz)', color: 'black' },
+    { id: '로버트 드 니로(Robert DeNiro)', color: 'black' },
+    { id: '워너브러더스 코리아(주)', color: 'purple' },
+    { id: '워너브러더스 코리아(주)', color: 'purple' },
+  ],
+  links: [
+    { source: '조커(Joker)', target: '장르' },
+    { source: '조커(Joker)', target: '개봉일' },
+    { source: '조커(Joker)', target: '감독' },
+    { source: '조커(Joker)', target: '배우' },
+    { source: '조커(Joker)', target: '회사' },
+    { source: '개봉일', target: '20191002' },
+    { source: '장르', target: '액션' },
+    { source: '장르', target: '드라마' },
+    { source: '장르', target: '스릴러' },
+    { source: '감독', target: '토드 필립스' },
+    { source: '배우', target: '호아킨 피닉스(Joaquin Phoenix)' },
+    { source: '배우', target: '재지 비츠(Zazie Beetz)' },
+    { source: '배우', target: '로버트 드 니로(Robert DeNiro)' },
+    { source: '회사', target: '워너브러더스 코리아(주)' },
+    { source: '회사', target: '워너브러더스 코리아(주)' },
+  ],
+};
 const data = {
   nodes: [
     {
@@ -56,6 +92,7 @@ const data = {
     { id: '(주)외유내강' },
     { id: '(주)필름케이' },
     { id: '20190731' },
+    { id: '공조' },
   ],
   links: [
     { source: '엑시트', target: '장르' },
@@ -69,6 +106,7 @@ const data = {
     { source: '감독', target: '이상근' },
     { source: '배우', target: '조정석' },
     { source: '배우', target: '윤아' },
+    { source: '윤아', target: '공조' },
     { source: '배우', target: '고두심' },
     { source: '제작사', target: '(주)외유내강' },
     { source: '제작사', target: '(주)필름케이' },
@@ -77,7 +115,9 @@ const data = {
 
 const DetailVisualization = (props) => {
   const { graphData } = props;
-  // console.log(data);
+  console.log(graphData);
+  console.log(JSON.stringify(graphData));
+
   return (
     <Graph
       id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
